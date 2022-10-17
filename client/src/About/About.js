07f992e7b1c31bf4411dd0f704adce0bf137af5a
lee <dudgk0216@naver.com>
2022-10-17 21:56:32 +0900

    first commit

---
+++ client/src/About/About.js
@@ -0,0 +1,162 @@
+import React, { useState } from 'react';
+import './About.css'
+
+const About = () => {
+
+  const [skill,setSkill] =useState([{name:"HTML,CSS,REACT",gage:"80%",progress:'progress-bar',
+ progressbg:"progress-bg"},
+     {name:"Js,REDUX",gage:"75%",
+     progress:'progress-bar1',
+     progressbg:"progress-bg1"},{name:"MONGODB,NODEJS",gage:"60%",
+     progress:'progress-bar2',
+     progressbg:"progress-bg2"}])
+
+
+    const progressBar = () =>{
+      return (
+        <div style={{display:"grid"}}>
+          {
+            skill.map((item)=>{
+              return(
+                <div style={{margin:"30px"}}>
+            
+                <h3>  {item.name}  {item.gage}</h3>
+              
+                <div>
+            
+                <div class={item.progressbg}>
+             
+            <div class={item.progress}>
+        
+                <h3 class="raised"></h3>
+              </div>
+                
+                <h3 class="goal"></h3>
+          </div>
+                  
+                </div>
+                </div>
+              )
+            
+            })
+          }
+               
+
+        </div>
+      )
+    }
+    
+    return (
+        <div className='About_font'>
+      
+         
+                    <div style={{display:"flex",justifyContent:"center",}}>
+           <div style={{width:"80%",height:"600px",margin:'50px'}}>
+           <h1>SKILLS</h1>
+            
+       {
+        progressBar()
+       }
+       
+     
+
+
+
+                </div>
+            </div>
+
+
+        <div className="wrap" style={{height:"500px"}}>
+        <div class="tile"> 
+          <img src='./image/포폴1.jpg'/>
+        
+          <div class="text">
+          <h1 >patience</h1>
+          
+          
+         <h2 class="animate-text">patience</h2>
+          <p class="animate-text">何がを勉強することが好きです。
+      そのおかげで、今まで決まった業務や資格を最後まで最善を尽くして、
+      良い結果を達しました。 </p>
+          <h2 style={{textAlign:"center"}}></h2>
+        <div class="dots">
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+          </div>
+        </div>
+
+        <div class="tile"> 
+          <img src='./image/포폴4.jpg'/>
+          <div class="text">
+          <h1>challenge spirit</h1>
+          <h2 class="animate-text">challenge spirit</h2>
+          <p class="animate-text">새로운것을 배우고 알아가는것을 좋아하고 제자신이 성장하는것을 좋아하는사람입니다.
+          코딩을 시작한것은 2022년 3월 부터 였습니다. 2021년도에 대학을 졸업하고 일본 지상직에 내정이 되었지만,
+           코로나로 인하여 무한 대기 상태에 놓여있었고 쉬는 동안 자격증을 따며 대기 하던도중, 개발자 동생에게 
+           생활코딩을 추천받았습니다.그때 저는 취미로 시작하였으나 코딩이 너무 재밌어서 진로를 변경하였고,
+           독학으로 지금까지 공부중 입니다.</p>
+          <h1></h1>
+        <div class="dots">
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+          </div>
+        </div>
+        <div class="tile"> 
+          <img src='./image/포폴2.jpg'/>
+          <div class="text">
+         <h1>
+         problem-solving ability
+         </h1>
+        <h2 class="animate-text"> 문제해결을 좋아하는사람</h2>
+          <p class="animate-text">무언가를 끝까지 해내고 난뒤에 느끼는 성취감이 너무 좋습니다.
+           프로젝트를 제작하거나, 코딩 연습할때, 오류가 나는 경우가 굉장히 많았습니다. 그때 스스로 
+           검색하고 공부하면서 해결해낸후 느끼는 성취감이나, 제가 머리속으로 상상한것이 직접 코딩을 함으로써
+           결과물로 나타나는것이 매우 좋습니다.</p>
+          {/* <h1>Lorem ipsum.</h1> */}
+        <div class="dots">
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+          </div>
+        </div>
+
+</div>
+
+
+
+
+
+
+
+
+
+           {/* <div style={{margin:"150px",display:"flex",justifyContent:'center',}}>
+            <div style={{
+            width:"80%",height:"300px",display:'flex',justifyContent:"space-around",}}>
+              <span>
+              <img alt='증명사진' src='image/증명사진.jpg'
+                  style={{width:'200px',height:"300px"}}></img>   
+              </span>
+                
+                  <span>
+                    <h2>프로필</h2>
+                  
+                    <h3>2019.02~2021.02 한국방송통신대학교 관광학과 졸업</h3>
+                    <h3>Jlpt n1,TOEIC 620,컴퓨터활용능력 2급 보유</h3>
+                   
+                    </span>   
+            </div>
+        
+           </div> */}
+       
+       
+        </div>
+    );
+};
+
+export default About;
---
+++ client/src/History/History.js
@@ -0,0 +1,162 @@
+import React, { useState } from 'react';
+import './History.scss'
+
+const History = () => {
+
+    const [next,setNext] =useState(1);
+    const [next2,setNext2] =useState(false)
+
+    const NextHandler = () =>{
+        setNext(next+1)
+    }
+    const prevHandler = () =>{
+        setNext(next-1)
+    }
+    const NextHandler2 = () =>{
+        setNext2(true)
+    }
+    const prevHandler2 = () =>{
+        setNext2(false)
+    }
+    const secondPageRenderer = () =>{
+        return(
+            <div style={{height:'500px',
+            }}>
+               <h4>プロジェットの説明</h4>
+                <h5>  
+                    専攻が観光学科だったので、旅行に関したアプリを制作したかったです。
+                    ユーザーと旅行の計画を一緒に作ってみて、観光地、宿泊施設、美味しい店、
+                    ショッピングスポットで分けて見るのができて、おすすめとかいいね機能もあります。
+                    そして、 Hellojejuのユーザーがお互いに情報を共有したり、観光地を推薦することができる
+                    コミュニティもあります。
+
+                </h5>
+      
+           <h5> 
+            TRAVELSPOTPage: 韓国のJEJUと言う観光地を分けて、宿泊施設、美味しい店、ショッピングスポットのカテゴリーで検索ができるし、行きたいスポットはいいねをして、
+             いいねリストで見ることができます。
+           </h5>
+          <h5>
+          NEWSPAGE:私が直接に行って推薦したかった場所を動画で制作して推薦しています。ページの下には観光地、
+          美味しい店、宿泊施設、ショッピングスポットで分けて、googleのapiを使用して、推薦したい場所を地図に
+　　　　　見せています。
+          </h5>
+           
+            <button style={{fontSize:"1.2rem"}}className='btn' onClick={prevHandler}>prev</button>
+            <button style={{fontSize:"1.2rem"}}className='btn' onClick={NextHandler}>Next</button>
+            </div>
+        )
+    }
+   
+    const hellojejuthird = () =>{
+        return (
+            <div style={{height:'500px'}}>
+ <h5> 
+            community Page: Hellojejuのユーザーがお互いに情報を共有したり、観光地を推薦することができる
+            コミュニティPageです。
+        
+           </h5>
+          <h5>
+          MyTravel: ユーザーの情報を見ることができるし、旅行のスケジュールを直接に設定して、追加、削除ができます。
+       
+          </h5>
+           
+            <button style={{fontSize:"1.2rem"}} className='btn' onClick={prevHandler}>prev</button>
+            </div>
+        )
+    }
+    const secondPageRenderer2 = ( ) =>{
+        return(
+            <div style={{height:'500px'}}>
+                <h4>프로젝트 설명</h4>
+             
+                <h5>
+                    ヴィンテージ商品のショッピングモールで、ユーザーが商品をアップロードをしたり、購入もできます。
+                    お支払いはpaypalの　テストpayで仮想のお支払いもできます。
+                </h5>
+                 <button style={{fontSize:"1.2rem"}} className='btn'onClick={prevHandler2}>prev</button>
+            </div>
+        )
+    }
+    return (
+        <div style={{width:"100%"}} >
+            {/* 헬로우 제주 */}
+            <div style={{display:"flex",justifyContent:"center",}}>
+            <div style={{width:"80%",height:'600px',}}>
+          <div style={{display:"flex",justifyContent:"space-around",margin:"50px"}}>
+          <video style={{width:'600px',height:"400px",position:"absolute",
+        left:"15%" }} id="vid" src="video/헬로우 제주.mp4" controls></video>
+
+          <div style={{width:"350px",position:"absolute",
+        left:"65%"}}>
+          
+          {
+            next===1?<div style={{}}>
+                <h2>HELLO JEJU</h2>
+                 <h5>制作期間:　2022.06.01~2022.07.31 2っか月</h5>
+           <h5> 言語: HTML,CSS,JS,REACT,MONGODB,NODEJS</h5>
+           <h5>githubで cloneして、terminalで  npm install, そして、cd clientした後にnpm install、
+                最後に　npm run dev 入力してくださったら、テストができます。 🙆‍♀️</h5>
+           <h5>
+           <a  href='https://github.com/leeyounagh/Newhellojeju'>
+            最後の完成作はこちらにあります。</a>
+          
+           </h5>
+        
+           <button style={{fontSize:"1.2rem"}}className='btn' onClick={NextHandler}>
+              Next
+           </button>
+           </div>:next===2?<div>{secondPageRenderer()}</div>:<div>{hellojejuthird()}</div>
+          }
+          
+            
+            {/* 2번째베이지 */}
+            {/*  */}
+          </div>
+        
+          </div>
+           
+        
+          
+     
+            </div>
+
+            </div>
+
+
+            {/* 쇼핑몰 */}
+            <div style={{
+       display:"flex",justifyContent:"center",height:'100%',}}>
+            <div style={{width:"80%",height:'600px'}}>
+             <div style={{display:"flex",justifyContent:"space-around"}}>
+             <video style={{width:'600px',height:"400px",position:"absolute",
+        left:"15%"  }} id="vid" src="video/쇼핑몰.mov" controls></video>
+             <div style={{width:"350px",marginTop:"50px",position:"absolute",
+        left:"65%"}}>
+                {
+                    next2?<div>{secondPageRenderer2()}</div>:
+                   <div style={{height:'500px',}}>
+                    <h2>VintAge</h2>
+                       <h5>制作期間:2022.05.01~05.31 一っか月</h5>
+              <h5> 사용언어: HTML,CSS,JS,REACT,MONGODB,NODEJS</h5>
+        
+            <h5>githubで cloneして、terminalで  npm install, そして、cd clientした後にnpm install、
+                最後に　npm run dev 入力してくださったら、テストができます。 🙆‍♀️
+                
+            </h5>
+            <h5><a href='https://github.com/leeyounagh/shoppingmall'>github</a></h5>
+            <button style={{fontSize:"1.2rem"}} className='btn' onClick={NextHandler2}>Next</button>
+                   </div>
+                }
+           
+                </div>
+
+             </div>
+            </div>
+            </div>
+    
+        </div>
+    );
+};
+
+export default History;
